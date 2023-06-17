70deb878e69208ab8f5d1138dd13272286ea9038
thienluhoan <thienluhoan@outlook.com>
2023-06-17 20:47:29 +0700

    Ver1.1

--- Asynchronous - USART.c
+++ Asynchronous - USART.c
@@ -10,12 +10,11 @@ void UART_init(unsigned int ubrr){
 	UBRR0H = (unsigned char)(ubrr>>8);
 	UBRR0L = (unsigned char)ubrr;
 	
-	//set frame format:
+	//set frame format: 1 bit stop, 8 bit data
+	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);		// or UCSR0C = 0x06;
 	
-	UCSR0C = 0x06;
-	//UCSR0C = (1<<USBS0) | (3<<UCSZ00);
 	//enable receiver and transmitter:
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0); 
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
 	
 }
 void Data_Tx(unsigned char data){
@@ -28,10 +27,14 @@ int main(void)
 {
 	UART_init(MYUBRR);
 	while(1){
-		for (unsigned char i = 97; i<122; i++){
+			Data_Tx('T');
+			Data_Tx('h');
+			Data_Tx('i');
+			Data_Tx('e');
+			Data_Tx('n');
+			Data_Tx(' ');
+			_delay_ms(1000);
 			
-			Data_Tx(i);
-			_delay_ms(100);
-		}
+		
 	}
 }
